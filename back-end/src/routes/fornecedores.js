38d6c5dce3d086908f4ed2d1cc0998c9124e6e4f
moscardini-carvalho <moscardinicarvalho1234@gmail.com>
2025-03-31 21:50:16 -0300

    (31/03) Término dos CRUDs de Categoria e Fornecedor

--- back-end/src/routes/fornecedores.js
+++ back-end/src/routes/fornecedores.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import controller from "../controllers/categorias.js";
+import controller from "../controllers/Fornecedores.js";
 
 const router = Router()
 
 router.post('/', controller.create)
-router.get('/', controller.retrieveAll)
+router.get('/', controller.orderBy)
 router.get('/:id', controller.retrieveOne)
 router.put('/:id', controller.update)
 router.delete('/:id', controller.delete)
