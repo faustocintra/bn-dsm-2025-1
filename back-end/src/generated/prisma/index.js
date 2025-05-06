7f79a7dc2b56888799f668415e76189658066a9f
ThiagoDav <davanzothi@gmail.com>
2025-05-05 22:11:52 -0300

    (05/05) CRUDs de Pedido e ItemPedido

--- back-end/src/generated/prisma/index.js
+++ back-end/src/generated/prisma/index.js
@@ -121,7 +121,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
