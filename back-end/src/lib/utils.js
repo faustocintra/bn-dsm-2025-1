28ff9614b507de15173e5d28797d6eeb24159251
Lívia Portela Ferreira <liviaportelaferreira@gmail.com>
2025-05-05 21:53:33 -0300

    (05/05) CRUDs de Pedido e ItemPedido

--- back-end/src/lib/utils.js
+++ back-end/src/lib/utils.js
@@ -6,20 +6,28 @@
 */
 function includeRelations(query) {
 
-    // Por padrão, não inclui nenhum relacionamento
-    const include = {}
+  // Por padrão, não inclui nenhum relacionamento
+  const include = {}
 
-    // Se o parâmetro "include" estiver na query string
-    if (query.include) {
-        // Recorta o valor do parâmetro, separando os
-        // relacionamentos informados onde há vírgula
-        const relations = query.include.split(',')
+  // Se o parâmetro "include" estiver na query string
+  if (query.include) {
+    // Recorta o valor do parâmetro, separando os
+    // relacionamentos informados onde há vírgula
+    const relations = query.include.split(',')
 
-        // Preenche a const "include" com as relações informadas
-        for (let rel of relations) include[rel] = true
+    // Preenche a const "include" com as relações informadas
+    for (let rel of relations) {
+      // Include de 2° nível (único caso nesta aplicação)
+      if (rel === 'itens.produto') {
+        include.itens = {
+          include: { produto: true }
+        }
+      }
+      else include[rel] = true
     }
+  }
 
-    return include
+  return include
 }
 
 export { includeRelations }
