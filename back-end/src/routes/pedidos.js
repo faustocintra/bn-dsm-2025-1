a9b57cf7bb8490fe89cde34ae8b3dd76b2ae47f6
Thiago Archete <162491020+ThiagoArchete@users.noreply.github.com>
2025-05-12 22:23:57 -0300

    (12/05) Primeira parte do trabalho 1 sem prints

--- back-end/src/routes/pedidos.js
+++ back-end/src/routes/pedidos.js
@@ -7,7 +7,7 @@ router.post('/', controller.create)
 router.get('/', controller.retrieveAll)
 router.get('/:id', controller.retrieveOne)
 router.put('/:id', controller.update)
-router.put('/:id', controller.delete)
+router.delete('/:id', controller.delete)
 
 
 // Rotas para os itens do pedido
