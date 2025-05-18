0d3db14f4f234168b7f06b2ceb433bd35637f51c
Eduardo Batista <dudubmatos@gmail.com>
2025-05-18 20:06:12 -0300

    Entrega Final Trabalho

--- back-end/src/controllers/clientes.js
+++ back-end/src/controllers/clientes.js
@@ -1,4 +1,5 @@
 import prisma from '../database/client.js'
+import { includeRelations } from '../lib/utils.js'
 
 const controller = {}   // Objeto vazio
 
@@ -8,7 +9,7 @@ controller.create = async function(req, res) {
      dentro de req.body
   */
   try {
-    await prisma.clientes.create({ data: req.body })
+    await prisma.cliente.create({ data: req.body })
 
     // Envia uma mensagem de sucesso ao front-end
     // HTTP 201: Created
@@ -17,7 +18,7 @@ controller.create = async function(req, res) {
   catch(error) {
     // Deu errado: exibe o erro no terminal
     console.error(error)
-	
+
     // Envia o erro ao front-end, com status de erro
     // HTTP 500: Internal Server Error
     res.status(500).send(error)
@@ -25,9 +26,13 @@ controller.create = async function(req, res) {
 }
 
 controller.retrieveAll = async function(req, res) {
+
+  const include = includeRelations(req.query)
+
   try {
     // Manda buscar os dados no servidor de BD
-    const result = await prisma.clientes.findMany({
+    const result = await prisma.cliente.findMany({
+      include,
       orderBy: [ { nome: 'asc' } ]
     })
 
@@ -47,10 +52,14 @@ controller.retrieveAll = async function(req, res) {
 
 controller.retrieveOne = async function(req, res) {
   try {
+
+    const include = includeRelations(req.query)
+
     // Manda buscar o documento no servidor de BD
     // usando como critério de busca um id informado
     // no parâmetro da requisição
-    const result = await prisma.clientes.findUnique({
+    const result = await prisma.cliente.findUnique({
+      include,
       where: { id: req.params.id }
     })
 
@@ -74,7 +83,7 @@ controller.update = async function(req, res) {
     // Busca o documento pelo id passado como parâmetro e,
     // caso o documento seja encontrado, atualiza-o com as
     // informações passadas em req.body
-    await prisma.clientes.update({
+    await prisma.cliente.update({
       where: { id: req.params.id },
       data: req.body
     })
@@ -103,7 +112,7 @@ controller.delete = async function(req, res) {
   try {
     // Busca o documento a ser excluído pelo id passado
     // como parâmetro e efetua a exclusão, caso encontrado
-    await prisma.clientes.delete({
+    await prisma.cliente.delete({
       where: { id: req.params.id }
     })
 
